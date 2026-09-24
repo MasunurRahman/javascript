e58ad70d266c1c5818801cb4585278e10d634dce
MasunurRahman <masunur.rahman.ap@gmail.com>
2026-09-24 15:58:35 +0600

    ci: 6 more files

---
+++ anisulIslam/T11personalBudgetTracker/index.js
@@ -0,0 +1,9 @@
+// Task Personal Budget Tracker app
+
+const userName = "Masunur Rahman";
+const income = 37000;
+const expense = 30000;
+
+console.log("User name : " + userName.toUpperCase());
+console.log("<br>Income : $" + income);
+console.log("<br>Expenses : $" + expense);
